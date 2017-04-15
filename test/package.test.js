5a74c9f7f7f2537f6f6e7c0d7ae10078da8ddfee
Chun-Kai Wang <chunkai1312@gmail.com>
2017-04-15 23:06:42 +0800

    test: *

--- test/package.test.js
+++ test/package.test.js
@@ -1,16 +1,12 @@
-/* global describe, it, expect */
+var strategy = require('..')
 
-var strategy = require('..');
+describe('passport-punwave', function () {
+  it('should export Strategy constructor', function () {
+    expect(strategy.Strategy).to.be.a('function')
+  })
 
-describe('passport-punwave', function() {
-    
-  it('should export Strategy constructor', function() {
-    expect(strategy.Strategy).to.be.a('function');
-  });
-    
-  it('should export Strategy constructor as module', function() {
-    expect(strategy).to.be.a('function');
-    expect(strategy).to.equal(strategy.Strategy);
-  });
-  
-});
+  it('should export Strategy constructor as module', function () {
+    expect(strategy).to.be.a('function')
+    expect(strategy).to.equal(strategy.Strategy)
+  })
+})
--- test/strategy.test.js
+++ test/strategy.test.js
@@ -1,117 +1,109 @@
-/* global describe, it, expect, before */
-/* jshint expr: true */
+/* eslint no-unused-vars: 0, no-unused-expressions: 0, standard/no-callback-literal: 0 */
 
 var chai = require('chai')
-  , PunwaveStrategy = require('../lib/strategy');
+var PunwaveStrategy = require('../lib/strategy')
 
-
-describe('Strategy', function() {
-    
-  describe('constructed', function() {
+describe('Strategy', function () {
+  describe('constructed', function () {
     var strategy = new PunwaveStrategy({
-        clientID: 'ABC123',
-        clientSecret: 'secret'
-      },
-      function() {});
-    
-    it('should be named punwave', function() {
-      expect(strategy.name).to.equal('punwave');
-    });
-  })
-  
-  describe('constructed with undefined options', function() {
-    it('should throw', function() {
-      expect(function() {
-        var strategy = new PunwaveStrategy(undefined, function(){});
-      }).to.throw(Error);
-    });
+      clientID: 'ABC123',
+      clientSecret: 'secret'
+    }, function () {})
+
+    it('should be named punwave', function () {
+      expect(strategy.name).to.equal('punwave')
+    })
   })
 
-  describe('authorization request with display parameter', function() {
+  describe('constructed with undefined options', function () {
+    it('should throw', function () {
+      expect(function () {
+        var strategy = new PunwaveStrategy(undefined, function () {})
+      }).to.throw(Error)
+    })
+  })
+
+  describe('authorization request with display parameter', function () {
     var strategy = new PunwaveStrategy({
-        clientID: 'ABC123',
-        clientSecret: 'secret'
-      }, function() {});
-    
-    
-    var url;
+      clientID: 'ABC123',
+      clientSecret: 'secret'
+    }, function () {})
 
-    before(function(done) {
+    var url
+
+    before(function (done) {
       chai.passport.use(strategy)
-        .redirect(function(u) {
-          url = u;
-          done();
+        .redirect(function (u) {
+          url = u
+          done()
         })
-        .req(function(req) {
+        .req(function (req) {
         })
-        .authenticate();
-    });
+        .authenticate()
+    })
 
-    it('should be redirected', function() {
-      expect(url).to.equal('https://api.punwave.com/oauth/authorize?response_type=code&client_id=ABC123');
-    });
-  });
-  
-  describe('failure caused by user denying request', function() {
+    it('should be redirected', function () {
+      expect(url).to.equal('https://api.punwave.com/oauth/authorize?response_type=code&client_id=ABC123')
+    })
+  })
+
+  describe('failure caused by user denying request', function () {
     var strategy = new PunwaveStrategy({
-        clientID: 'ABC123',
-        clientSecret: 'secret'
-      }, function() {});
-    
-    
-    var info;
-  
-    before(function(done) {
+      clientID: 'ABC123',
+      clientSecret: 'secret'
+    }, function () {})
+
+    var info
+
+    before(function (done) {
       chai.passport.use(strategy)
-        .fail(function(i) {
-          info = i;
-          done();
+        .fail(function (i) {
+          info = i
+          done()
         })
-        .req(function(req) {
-          req.query = {};
-          req.query.error = 'access_denied';
-          req.query.error_code = '200';
-          req.query.error_description  = 'Permissions error';
-          req.query.error_reason = 'user_denied';
+        .req(function (req) {
+          req.query = {}
+          req.query.error = 'access_denied'
+          req.query.error_code = '200'
+          req.query.error_description = 'Permissions error'
+          req.query.error_reason = 'user_denied'
         })
-        .authenticate();
-    });
-  
-    it('should fail with info', function() {
-      expect(info).to.not.be.undefined;
-      expect(info.message).to.equal('Permissions error');
-    });
-  });
-  
-  describe('error caused by invalid code sent to token endpoint', function() {
+        .authenticate()
+    })
+
+    it('should fail with info', function () {
+      expect(info).to.not.be.undefined
+      expect(info.message).to.equal('Permissions error')
+    })
+  })
+
+  describe('error caused by invalid code sent to token endpoint', function () {
     var strategy = new PunwaveStrategy({
-        clientID: 'ABC123',
-        clientSecret: 'secret'
-      }, function() {});
-      
-    strategy._oauth2.getOAuthAccessToken = function(code, options, callback) {
-      return callback({ error: 'invalid_grant', error_description: 'Invalid authorization code' });
-    };
-  
-  
-    var err;
+      clientID: 'ABC123',
+      clientSecret: 'secret'
+    }, function () {})
 
-    before(function(done) {
+    strategy._oauth2.getOAuthAccessToken = function (code, options, callback) {
+      return callback({ statusCode: 400, data: { error: 'invalid_grant', error_description: 'Invalid authorization code' } })
+    }
+
+    var err
+
+    before(function (done) {
       chai.passport.use(strategy)
-        .error(function(e) {
-          err = e;
-          done();
+        .error(function (e) {
+          err = e
+          done()
         })
-        .req(function(req) {
-          req.query = {};
-          req.query.code = 'SplxlOBeZQQYbYS6WxSbIA+ALT1';
+        .req(function (req) {
+          req.query = {}
+          req.query.code = 'SplxlOBeZQQYbYS6WxSbIA+ALT1'
         })
-        .authenticate();
-    });
+        .authenticate()
+    })
 
-    it('should error', function() {
-      expect(err.constructor.name).to.equal('InternalOAuthError');
-    });
-  }); // error caused by invalid code sent to token endpoint
-  
-});
+    it('should error', function () {
+      expect(err.constructor.name).to.equal('InternalOAuthError')
+    })
+  }) // error caused by invalid code sent to token endpoint
+})
